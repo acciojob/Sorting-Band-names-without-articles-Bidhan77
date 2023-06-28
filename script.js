f5e2d884a10ea553a8e76bcdda4b5840f7b5fa81
Bidhan Rakshit <90621019+Bidhan77@users.noreply.github.com>
2023-06-28 13:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
 
+function rot13(encodedStr) {
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for (let i = 0; i < encodedStr.length; i++) {
+	let char = encodedStr[i];
+    let decodedChar = lookup[char] || char;
+    decodedArr.push(decodedChar);
+  }
+
+  return decodedArr.join("");
+}
