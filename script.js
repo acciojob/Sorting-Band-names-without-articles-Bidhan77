09e200e7b4aca1fe35274107866cb8ea05907ff7
Bidhan Rakshit <90621019+Bidhan77@users.noreply.github.com>
2023-06-29 14:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-//your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Red Hot Chili Peppers', 'The Rolling Stones', 'Aerosmith'];
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for (let i = 0; i < encodedStr.length; i++) {
-	let char = encodedStr[i];
-    let decodedChar = lookup[char] || char;
-    decodedArr.push(decodedChar);
-  }
+// Function to remove articles from the band names
+function removeArticles(name) {
+  const articles = ['a', 'an', 'the'];
+  const words = name.split(' ');
 
-  return decodedArr.join("");
+  // Remove articles from the band name
+  const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+
+  return filteredWords.join(' ');
+}
+
+// Sort the band names in lexicographic order excluding articles
+bandNames.sort((a, b) => {
+  const nameA = removeArticles(a);
+  const nameB = removeArticles(b);
+  return nameA.localeCompare(nameB);
+});
+
+// Display the sorted band names inside the <ul id="band"> tag
+const ulElement = document.getElementById('band');
+for (let i = 0; i < bandNames.length; i++) {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandNames[i];
+  ulElement.appendChild(liElement);
 }
